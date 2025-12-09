729df56145ca35e900f62bea862e6b06edd3d426
Tawanda Nduna Mashoko <mashokotawanda61@gmail.com>
2025-12-09 17:52:49 +0100

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,33 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements){
+        if(linkElement.href.slice(0, 1) === '/') {
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch(err){
+                console.log(`error with relative url: ${err.message}`)
+            }
+            
+        } else {
+            //absolute
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch(err){
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
+
 // this allows us to see what is going in and what is going out{in the return function}
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
@@ -9,5 +39,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
